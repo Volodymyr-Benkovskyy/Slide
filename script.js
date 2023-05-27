48ac2dbe810208a2048a0185eb3fc2dd916ec69d
Volodymyr-Benkovskyy <123126442+Volodymyr-Benkovskyy@users.noreply.github.com>
2023-05-27 20:13:41 +0200

    create slider

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
   direction: "vertical",
-  /*  loop: true, */
+  loop: true,
 
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-
   spaceBetween: 50,
-  /*  slidesPerView: 1,
-   */
+  slidesPerView: 1,
+
   initialSlide: 0,
 });
-console.log(Swiper);
+
+const swiperaPagination = document.querySelector(".swiper-pagination");
+const btnColor = document.querySelector(".js-btn-color");
+
+const onSwiper = (event) => {
+  const { target: bullet } = event;
+  const ariaLabel = bullet.getAttribute("aria-label");
+  let color = "";
+
+  if (ariaLabel === "Go to slide 1") {
+    color = "hsl(237, 42%, 35%)";
+    btnColor.style.background = color;
+  } else if (ariaLabel === "Go to slide 2") {
+    color = "hsl(0, 92%, 20%)";
+    btnColor.style.background = color;
+  } else if (ariaLabel === "Go to slide 3") {
+    color = "hsl(92, 36%, 37%)";
+    btnColor.style.background = color;
+  }
+};
+
+swiperaPagination.addEventListener("click", onSwiper);
